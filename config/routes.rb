3bd1e924c8bb0c9cf73c27e56f31a17f1797571a
Marc-Antoine Blais <blaisma@live.fr>
2022-05-10 19:11:15 -0400

    CRUD methods are working as expected, some css done for nice display, navigation on all page is completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/', to: 'articles#index'
+  get '/articles', to: 'articles#index', as: :articles
+  get 'articles/new', to: 'articles#new', as: :new_article
+  get 'articles/:id', to: 'articles#show', as: :article
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  post 'articles', to: 'articles#create'
+  patch 'articles/:id', to: 'articles#update'
+  delete 'articles/:id', to: 'articles#destroy'
 end
